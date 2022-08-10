f04ef9575e904f05e6e2f8cdbd4d3db53a978d63
Ronaldo Silva <ronaldojssilva@gmail.com>
2022-08-10 12:22:27 -0400

    Cria produtos controller

---
+++ ApiCatalago/Controllers/ProdutosController.cs
@@ -0,0 +1,18 @@
+﻿using ApiCatalago.Context;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiCatalago.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ProdutosController : ControllerBase
+    {
+        private readonly ApiDbContext _context;
+
+        public ProdutosController(ApiDbContext context)
+        {
+            _context = context;
+        }
+    }
+}
