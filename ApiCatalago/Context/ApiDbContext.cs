4e1ceffec0cd44c548c2b598dfc0b2b50e2cfbf9
Ronaldo Silva <ronaldojssilva@gmail.com>
2022-08-09 23:12:12 -0400

    Contexto

---
+++ ApiCatalago/Context/ApiDbContext.cs
@@ -0,0 +1,16 @@
+﻿using ApiCatalago.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiCatalago.Context
+{
+    public class ApiDbContext: DbContext
+    {
+        public ApiDbContext(DbContextOptions<ApiDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Categoria>? Categorias { get; set; }
+        public DbSet<Produto>? Produtos { get; set; }
+    }
+}
