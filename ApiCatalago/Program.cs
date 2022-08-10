c47ee0d2876c0734f3579dd982fd4dfa25fc23ed
Ronaldo Silva <ronaldojssilva@gmail.com>
2022-08-09 23:28:56 -0400

    Registro serviço

--- ApiCatalago/Program.cs
+++ ApiCatalago/Program.cs
@@ -1,3 +1,6 @@
+using ApiCatalago.Context;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+string mySqlConnection = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<ApiDbContext>(options => options.UseMySql(mySqlConnection, ServerVersion.AutoDetect(mySqlConnection)));
 
 var app = builder.Build();
 
