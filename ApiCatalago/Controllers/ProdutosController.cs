21c4f82c37ea1bf78201e35a07a2b005db3776fa
Ronaldo Silva <ronaldojssilva@gmail.com>
2022-08-10 14:23:20 -0400

    Produtos Controller Put

--- ApiCatalago/Controllers/ProdutosController.cs
+++ ApiCatalago/Controllers/ProdutosController.cs
@@ -2,6 +2,7 @@
 using ApiCatalago.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace ApiCatalago.Controllers
 {
@@ -49,6 +50,20 @@ namespace ApiCatalago.Controllers
             _context.SaveChanges();
 
             return new CreatedAtRouteResult("ObterProduto", new {id = produto.ProdutoId}, produto);
+        }
+
+        [HttpPut("{id:int}")]
+        public ActionResult Put(int id, Produto produto)
+        {
+            if (id != produto.ProdutoId)
+            {
+                return BadRequest();
+
+            }
+            _context.Entry(produto).State = EntityState.Modified;
+            _context.SaveChanges();
+
+            return Ok(produto);
         }
     }
 }
