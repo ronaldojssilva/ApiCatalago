76353a9a37b1f3abb6131c00785b628b5f716faa
Ronaldo Silva <ronaldojssilva@gmail.com>
2022-08-10 14:05:54 -0400

    Produtos Controller Post

--- ApiCatalago/Controllers/ProdutosController.cs
+++ ApiCatalago/Controllers/ProdutosController.cs
@@ -26,7 +26,7 @@ namespace ApiCatalago.Controllers
             return produtos;
         }
 
-        [HttpGet("{id:int}")]
+        [HttpGet("{id:int}", Name="ObterProduto")]
         public ActionResult<Produto> Get(int id)
         {
             var produto = _context.Produtos.FirstOrDefault(p => p.ProdutoId == id);
@@ -35,6 +35,20 @@ namespace ApiCatalago.Controllers
                 return NotFound("Produto não encontrado!");
             }
             return produto;
+        }
+
+        [HttpPost]
+        public ActionResult Post(Produto produto)
+        {
+            if (produto is null)
+            {
+                return BadRequest();
+            }
+
+            _context.Produtos.Add(produto);
+            _context.SaveChanges();
+
+            return new CreatedAtRouteResult("ObterProduto", new {id = produto.ProdutoId}, produto);
         }
     }
 }
